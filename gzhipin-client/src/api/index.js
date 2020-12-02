cf597cb395bb28c39043a622fcb09116958f880f
yujunjie0603 <yu.long.fr@gmail.com>
2020-12-02 23:02:10 +0100

    new fichier

---
+++ gzhipin-client/src/api/index.js
@@ -0,0 +1,5 @@
+import ajax from './ajax'
+
+export const reqRegister = (user) => ajax('/register', user, 'POST') 
+export const reqLogin = ({username, password}) => ajax('/login', {username, password}, 'POST') 
+export const reqUpdateUser = (user) => ajax('/update', user, 'POST') 
